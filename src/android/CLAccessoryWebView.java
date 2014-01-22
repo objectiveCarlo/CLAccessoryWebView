cd219f5cff4d4c918101d5a9cb23e9bca9a145fa
Carlo Luis Bation <cbation@isbx.com>
2014-01-22 16:57:27 +0800

    Finished AccessoryWebView

--- src/android/CLAccessoryWebView.java
+++ src/android/CLAccessoryWebView.java
@@ -5,35 +5,135 @@ import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONException;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.WindowManager;
 import android.webkit.WebView;
-import android.widget.LinearLayout;
+import android.webkit.WebViewClient;
 
+@SuppressLint("NewApi")
 public class CLAccessoryWebView extends CordovaPlugin {
 
-     public WebView accessoryWebview;
-     
-	 public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
-	        if (action.equals("open")) {
-	            final String url = args.getString(0);
-	            Double xPosition = args.getDouble(1);
-	            Double yPosition = args.getDouble(2);
-	            accessoryWebview = new WebView(this.cordova.getActivity());
-	            accessoryWebview.loadUrl(url);
-	            this.webView.addView(accessoryWebview);
-	            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-	            	     LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-	            layoutParams.setMargins(xPosition.intValue(), yPosition.intValue(), 0, 0);
-	            this.webView.setLayoutParams(layoutParams);
-	            return true;
-	        }else{
-	        	if (action.equals("close")) {
-		            if(accessoryWebview != null)
-		            	this.webView.removeView(accessoryWebview);
-		            
-		            return true;
-		        }
-	        }
-	        
-	        return false;
-	 }
+	public static final String METHOD_OPEN  = "open";
+	public static final String METHOD_CLOSE = "close";
+
+
+	public WebView accessoryWebview;
+	public CLAccessoryWebViewClient webClient;
+	public String originalUrl = null;
+
+
+	@SuppressLint("SetJavaScriptEnabled")
+	public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+		if (action.equals(METHOD_OPEN)) {
+			originalUrl = args.getString(0);
+			final Double xPosition = args.getDouble(1);
+			final Double yPosition = args.getDouble(2);
+			final Double height = args.getDouble(3);
+			Runnable runnable = new Runnable() {
+				public void run() {
+
+					CLAccessoryWebView.this.close();
+
+					if(accessoryWebview == null){
+						accessoryWebview = new WebView(CLAccessoryWebView.this.cordova.getActivity());
+						webClient = new CLAccessoryWebViewClient();
+						accessoryWebview.setWebViewClient(webClient);
+					}
+
+					WindowManager.LayoutParams windowParams = new WindowManager.LayoutParams();
+
+					windowParams.height = height.intValue();
+					windowParams.width =  WindowManager.LayoutParams.MATCH_PARENT;
+					windowParams.x = xPosition.intValue();
+					windowParams.y = yPosition.intValue();
+					windowParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+							| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+							| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+							| WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+							| WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
+					windowParams.format = PixelFormat.TRANSLUCENT;
+					windowParams.windowAnimations = 0;
+					windowParams.gravity = Gravity.BOTTOM;
+					windowParams.verticalMargin = yPosition.intValue();
+					ViewParent parent = webView.getParent();
+
+					if(parent != null && parent instanceof ViewGroup){
+						ViewGroup viewGroup = (ViewGroup)parent;
+						viewGroup.addView(accessoryWebview,windowParams);
+					}else{
+						CLAccessoryWebView.this.webView.addView(accessoryWebview,windowParams);
+					}
+
+					accessoryWebview.clearCache(true);
+					accessoryWebview.clearHistory();
+					accessoryWebview.getSettings().setJavaScriptEnabled(true);
+					accessoryWebview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+					accessoryWebview.getSettings().setLoadWithOverviewMode(true);
+					accessoryWebview.getSettings().setUseWideViewPort(true);
+					accessoryWebview.loadUrl(originalUrl); 
+
+				};
+			};
+			this.cordova.getActivity().runOnUiThread(runnable);
+			return true;
+		}else{
+			if (action.equals(METHOD_CLOSE)) {
+				runCloseOnUIThread();
+				return true;
+			}
+		}
+
+		return false;
+	}
+	public void runCloseOnUIThread(){
+		Runnable runnable = new Runnable() {
+			public void run() {
+
+				CLAccessoryWebView.this.close();
+			};
+		};
+
+		this.cordova.getActivity().runOnUiThread(runnable);
+	}
+	public void close(){
+		if(accessoryWebview != null &&accessoryWebview.getParent() != null){
+			ViewGroup parentView = (ViewGroup) accessoryWebview.getParent(); 
+			((ViewGroup) parentView).removeView(accessoryWebview);
+		}
+
+	}
+
+	public class CLAccessoryWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			url = url.trim();
+			originalUrl = originalUrl.trim();
+			boolean contains = url.contains(originalUrl);
+			boolean equals = url == originalUrl;
+			boolean equalsWithSlash = url == (originalUrl+'/');
+
+
+			boolean decision = (contains || equals || equalsWithSlash);
+
+			if(originalUrl!=null && url != null && decision){ 
+				view.loadUrl(url);
+				return true;
+
+			} else {
+				view.getContext().startActivity(
+						new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+				return true; 
+			}
+		}
+		public void onPageFinished(WebView view, String url) {
+
+
+		}
+	}
 }
