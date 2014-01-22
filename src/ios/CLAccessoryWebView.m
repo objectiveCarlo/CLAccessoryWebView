790f48569f4a41f5da2ea03f909209fe0ad5fdd7
Carlo Luis Bation <cbation@isbx.com>
2014-01-22 18:19:54 +0800

    Blacklist in ios

--- src/ios/CLAccessoryWebView.m
+++ src/ios/CLAccessoryWebView.m
@@ -10,17 +10,18 @@
 
 @implementation CLAccessoryWebView
 - (void)open:(CDVInvokedUrlCommand*)command{
+    
     NSString* url = [command argumentAtIndex:0];
    float xPosition = [[command argumentAtIndex:1] floatValue];
    float yPosition = [[command argumentAtIndex:2] floatValue];
-    
+   float height    = [[command argumentAtIndex:3] floatValue];
     CGRect frame = self.webView.frame;
     frame.origin = CGPointMake(xPosition, yPosition);
-    
+    if(height > 0) frame.size.height = height;
     if(accesoryWebview != nil) [accesoryWebview removeFromSuperview];
     
     accesoryWebview = [[UIWebView alloc] initWithFrame:frame];
-
+    [accesoryWebview setDelegate:self];
     
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL
                                                                         
@@ -30,6 +31,7 @@
                                     
                                                        timeoutInterval:60.0];
     
+    originalUrlString = [[NSString alloc] initWithString:url];
     [accesoryWebview loadRequest:request];
     
     [self.webView addSubview:accesoryWebview];
@@ -37,5 +39,32 @@
 - (void)close:(CDVInvokedUrlCommand*)command{
     
     if(accesoryWebview !=nil) [accesoryWebview removeFromSuperview];
+}
+
+#pragma mark - UIWebViewDelegate methods
+
+- (BOOL) webView:(UIWebView *)inWeb shouldStartLoadWithRequest:(NSURLRequest *)inRequest navigationType:(UIWebViewNavigationType)inType {
+   
+    if ( inType == UIWebViewNavigationTypeLinkClicked ) {
+        [[UIApplication sharedApplication] openURL:[inRequest URL]];
+        return NO;
+    }
+    
+    NSURL* requestedURL = [inRequest URL];
+    
+    if(requestedURL != nil){
+        NSString* requestedURLString = [requestedURL absoluteString];
+        BOOL isEqual = [requestedURLString isEqualToString:originalUrlString];
+        BOOL containsOriginal = [requestedURLString rangeOfString:originalUrlString].location != NSNotFound;
+        BOOL containsRequested = [originalUrlString rangeOfString:requestedURLString].location != NSNotFound;
+        BOOL decision = isEqual || containsOriginal || containsRequested;
+        if(!decision){
+            [[UIApplication sharedApplication] openURL:requestedURL];
+            return NO;
+        }
+    }
+
+    
+    return YES;
 }
 @end
